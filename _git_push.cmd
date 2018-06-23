96930102b286164bd89f374538c91e7babff6a39
timebusker <timebusker@qq.com>
2018-06-23 18:07:53 +0800

    2018/06/23 18:07:53 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

--- _git_push.cmd
+++ _git_push.cmd
@@ -1,12 +1,8 @@
 color 0a
-@echo off & setlocal
-:: cd img/top-photo/
-:: call 0-rename-jpg.bat
-:: cd ..
-
+@echo off & setlocal 
 set var=":sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:"
 set d=%date:~0,10%
 set t=%time:~0,8%
 git add .
 git commit -am"%d% %t% ____ %var%"
-git push  origin master
+git push origin master
